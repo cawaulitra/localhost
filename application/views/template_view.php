5233f9b2ffd3742c066622c7e57afc1bbfd24ec2
cawaulitra <39634179+cawaulitra@users.noreply.github.com>
2021-05-19 10:46:56 +0300

    снёс админ-панель, заменил на две кнопочки

--- application/views/template_view.php
+++ application/views/template_view.php
@@ -270,7 +270,8 @@
 						//var_dump($_SESSION);
 						if ($_SESSION['id_role'] == '1') { //админ
 							echo '<li><a href="/">Главная</a></li>';
-							echo '<li><a href="/admin/main">Админ-панель</a></li>';
+							echo '<li><a href="/admin/tickets">Тикеты</a></li>';
+							echo '<li><a href="/admin/users">Пользователи</a></li>';
 							echo '<li><a href="/user/profile">Профиль</a></li>';
 						}
 
