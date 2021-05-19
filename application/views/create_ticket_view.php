6f2da38c1e5795f6101aff7c2a637413290f79f0
Poogdis <59322198+Poogdis@users.noreply.github.com>
2021-05-19 11:59:02 +0300

    Update create_ticket_view.php

--- application/views/create_ticket_view.php
+++ application/views/create_ticket_view.php
@@ -1,1 +1,59 @@
-Это страница создания тикета
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap" rel="stylesheet">
+<style>
+.content-create{
+    display: flex;
+    flex-direction: column;
+    align-content: center;
+    background: rgba(255, 255, 255, 0.95);
+    box-shadow: 0px 1px 49px rgba(0, 0, 0, 0.14);
+    width: 1294px;
+}
+.text{
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 50px;
+    line-height: 59px;
+
+    /* identical to box height */
+
+    color: #070651;
+
+}
+.plane{
+    display: flex;
+    flex-wrap: nowrap;
+    align-items: baseline;
+    justify-content: space-between;
+    padding: 0 50px;
+}
+.create_form{
+    display: flex;
+
+}
+.radio{
+    display: flex;
+}
+
+</style>
+<div class="content-create">
+    <div class="plane">
+        <p class="text">Создание тикета</p>
+    </div>
+    <div class="create_form">
+        <div class="main-opis">
+            <p>Название</p>
+            <input>
+            <p>Описание</p>
+            <textarea></textarea>
+            <p>Дополнительно</p>
+            <input type="file">
+        </div>
+        <div class="radio">
+            <p>Тип вопроса</p>
+            <input type="radio" value="dwadwad">
+            <input type="radio" value="dwadwad">
+        </div>
+    </div>
+</div>
